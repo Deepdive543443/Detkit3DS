7c0e14bc9a1898e7b87050a2085620fa2964904d
justinfung <qf28@sussex.ac.uk>
2024-02-03 18:18:35 +0000

    Add Cam to pixels function

--- src/main/cam.c
+++ src/main/cam.c
@@ -22,3 +22,24 @@ void writeCamToFramebufferRGB565(void *fb, void *img, u16 x, u16 y, u16 width, u
         }
     }
 }
+
+void writeCamToPixels(unsigned char *pixels, void *img, u16 x0, u16 y0, u16 width, u16 height)
+{
+    u16 *img_16 = (u16 *) img;
+    unsigned char *mat_ptr = pixels;
+    u16 data;
+
+    for (int j = y0; j < height; j++)
+    {
+        for (int i = x0; i < width; i++)
+        {
+            data = img_16[j * width + i];
+            
+            mat_ptr[0] = (unsigned char) ((data & 0x1F) << 3);
+            mat_ptr[1] = (unsigned char) (((data >> 5) & 0x3F) << 2);
+            mat_ptr[2] = (unsigned char) (((data >> 11) & 0x1F) << 3);
+
+            mat_ptr+=3;            
+        }
+    }
+}
--- src/main/sections.h
+++ src/main/sections.h
@@ -19,6 +19,7 @@
 #define WAIT_TIMEOUT 1000000000ULL
 
 void writeCamToFramebufferRGB565(void *fb, void *img, u16 x, u16 y, u16 width, u16 height);
+void writeCamToPixels(unsigned char *pixels, void *img, u16 x0, u16 y0, u16 width, u16 height);
 
 // display.c
 void writePic2FrameBuf565(void *fb, lv_color_t * color, u16 x, u16 y, u16 w, u16 h);
