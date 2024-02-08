9b9a1e6ab2da7bed155db0727e3c69aa63157425
justinfung <qf28@sussex.ac.uk>
2024-02-08 20:41:01 +0000

    Add makerom tool and resources(from 3DS cmake)

--- cmake/Tools3DS.cmake
+++ cmake/Tools3DS.cmake
@@ -39,6 +39,33 @@ if(NOT SMDHTOOL)
     endif()
 endif()
 
+#############
+## MAKEROM ##
+#############
+if(NOT MAKEROM)
+    # message(STATUS "Looking for makerom...")
+    # find_program(MAKEROM makerom ${DEVKITARM}/bin)
+    find_program(MAKEROM makerom /home/justinfung/Desktop/CIABUILDER/resources)
+    if(MAKEROM)
+        message(STATUS "makerom: ${MAKEROM} - found")
+    else()
+        message(WARNING "makerom - not found")
+    endif()
+endif()
+
+################
+## BANNERTOOL ##
+################
+if(NOT BANNERTOOL)
+    # message(STATUS "Looking for bannertool...")
+    find_program(BANNERTOOL bannertool /home/justinfung/Desktop/CIABUILDER/resources)
+    if(BANNERTOOL)
+        message(STATUS "bannertool: ${BANNERTOOL} - found")
+    else()
+        message(WARNING "bannertool - not found")
+    endif()
+endif()
+
 
 ###############
 ##  3DSLINK  ##
@@ -120,5 +147,78 @@ function(add_3dsx_target target)
         )
     endif()
     add_custom_target(${target_we}_3dsx ALL SOURCES ${CMAKE_CURRENT_BINARY_DIR}/${target_we}.3dsx)
+    set_target_properties(${target} PROPERTIES LINK_FLAGS "-specs=3dsx.specs")
+endfunction()
+
+# add cia Target
+function(__add_ncch_banner target IMAGE SOUND)
+    if(IMAGE MATCHES ".*\\.png$")
+        set(IMG_PARAM -i ${IMAGE})
+    else()
+        set(IMG_PARAM -ci ${IMAGE})
+    endif()
+    if(SOUND MATCHES ".*\\.wav$")
+        set(SND_PARAM -a ${SOUND})
+    else()
+        set(SND_PARAM -ca ${SOUND})
+    endif()
+    add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}
+                        COMMAND ${BANNERTOOL} makebanner -o ${CMAKE_CURRENT_BINARY_DIR}/${target} ${IMG_PARAM} ${SND_PARAM}
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                        DEPENDS ${IMAGE} ${SOUND}
+                        VERBATIM
+    )
+endfunction()
+
+function(add_cia_target target RSF IMAGE SOUND )
+    get_filename_component(target_we ${target} NAME_WE)
+    if(${ARGC} GREATER 6)
+        set(APP_TITLE ${ARGV4})
+        set(APP_DESCRIPTION ${ARGV5})
+        set(APP_AUTHOR ${ARGV6})
+    endif()
+    if(${ARGC} EQUAL 8)
+        set(APP_ICON ${ARGV7})
+    endif()
+    if(NOT APP_TITLE)
+        set(APP_TITLE ${target})
+    endif()
+    if(NOT APP_DESCRIPTION)
+        set(APP_DESCRIPTION "Built with devkitARM & libctru")
+    endif()
+    if(NOT APP_AUTHOR)
+        set(APP_AUTHOR "Unspecified Author")
+    endif()
+    if(NOT APP_ICON)
+        if(EXISTS ${target}.png)
+            set(APP_ICON ${target}.png)
+        elseif(EXISTS icon.png)
+            set(APP_ICON icon.png)
+        elseif(CTRULIB)
+            set(APP_ICON ${CTRULIB}/default_icon.png)
+        else()
+            message(FATAL_ERROR "No icon found ! Please use NO_SMDH or provide some icon.")
+        endif()
+    endif()
+    if( NOT ${target_we}.smdh)
+        __add_smdh(${target_we}.smdh ${APP_TITLE} ${APP_DESCRIPTION} ${APP_AUTHOR} ${APP_ICON})
+    endif()
+    __add_ncch_banner(${target_we}.bnr ${IMAGE} ${SOUND})
+    add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target_we}.cia
+                        # COMMAND ${STRIP} -o $<TARGET_FILE:${target}>-stripped $<TARGET_FILE:${target}>
+                        COMMAND ${MAKEROM}     -f cia
+                                            -target t
+                                            -exefslogo
+                                            -o ${CMAKE_CURRENT_BINARY_DIR}/${target_we}.cia
+                                            -elf $<TARGET_FILE:${target}>
+                                            -rsf ${RSF}
+                                            -banner ${CMAKE_CURRENT_BINARY_DIR}/${target_we}.bnr
+                                            -icon ${CMAKE_CURRENT_BINARY_DIR}/${target_we}.smdh
+                        DEPENDS ${target} ${RSF} ${CMAKE_CURRENT_BINARY_DIR}/${target_we}.bnr ${CMAKE_CURRENT_BINARY_DIR}/${target_we}.smdh
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                        VERBATIM
+    )
+
+    add_custom_target(${target_we}_cia ALL SOURCES ${CMAKE_CURRENT_BINARY_DIR}/${target_we}.cia)
     set_target_properties(${target} PROPERTIES LINK_FLAGS "-specs=3dsx.specs")
 endfunction()
