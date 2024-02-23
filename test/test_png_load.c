1079557b6d5e257083859accaf1a40f6828b31fb
justinfung <qf28@sussex.ac.uk>
2024-02-23 03:43:00 +0000

    try remove stb include

--- test/test_png_load.c
+++ test/test_png_load.c
@@ -5,12 +5,6 @@
 
 #include "lvgl/lvgl.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
 
 static struct timespec start, end;
 static lv_disp_t *disp_top;
