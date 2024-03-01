79008feea6a5145c5b1e6ef78b43faba2c9d3ca1
justinfung <qf28@sussex.ac.uk>
2024-03-01 14:46:40 +0000

    Revert "Script of prebuild ncnn for 3ds"
    This reverts commit bcbcf63fea79025b2a93ffe5e08ca543ba0ae1a2.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,3 @@ cd build
 wget https://github.com/lvgl/lvgl/archive/refs/tags/v8.3.11.zip
 unzip v8.3.11.zip
 mv lvgl-8.3.11 ../lib/lvgl-8.3.11
-
-wget https://github.com/Deepdive543443/detector_c/releases/download/20240229_master/ncnn_3ds_master.zip
-unzip ncnn_3ds_master.zip
-mv ncnn_3ds_master ../lib/ncnn_3ds_master
