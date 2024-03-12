52c62b4e09454c65cf71f74a6189f9c73d9e6f98
justinfung <qf28@sussex.ac.uk>
2024-03-12 16:40:35 +0000

    merge setup script

--- setup.sh
+++ setup.sh
@@ -6,4 +6,13 @@ mv lvgl-8.3.11 ../lib/lvgl-8.3.11
 
 wget https://github.com/Deepdive543443/detector_c/releases/download/20240229_master/ncnn_3ds_master.zip
 unzip ncnn_3ds_master.zip
-mv ncnn_3ds_master ../lib/ncnn_3ds_master 
+mv ncnn_3ds_master ../lib/ncnn_3ds_master
+
+cmake -DCMAKE_TOOLCHAIN_FILE="../cmake/DevkitArm3DS.cmake" ../lib/detector_c
+make -j6 install
+
+rm -rf *
+cmake -DCMAKE_TOOLCHAIN_FILE="../cmake/DevkitArm3DS.cmake" ../lib/lvgl
+make -j6 install
+
+rm -rf *
