f492118bc41ce0e51365edeb11dc3a46168ecad4
JustinFung <835776509@qq.com>
2024-04-16 21:36:50 +0800

    Update include dir and build script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,55 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+function build_module {  
+case "$1" in
+lvgl)
+    cd $SCRIPT_DIR/build
+    rm -rf *
+    wget https://github.com/lvgl/lvgl/archive/refs/tags/v8.3.11.zip
+    unzip v8.3.11.zip
+    mv lvgl-8.3.11 $SCRIPT_DIR/lib/lvgl/lvgl-8.3.11
+    
+    cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_DIR/cmake/DevkitArm3DS-toolchain.cmake" $SCRIPT_DIR/lib/lvgl
+    make -j6 install
+    mv install $SCRIPT_DIR/lib/build/lvgl
+    rm -rf *
+    ;;
+
+ncnn)
+    mkdir -p $SCRIPT_DIR/lib/build/ncnn_3ds
+    cd $SCRIPT_DIR/lib/build/ncnn_3ds
+    wget https://github.com/Deepdive543443/Benchncnn-3DS/releases/download/20240410/ncnn_3ds_20240410.zip
+    unzip ncnn_3ds_20240410.zip
+    rm ncnn_3ds_20240410.zip
+    ;;
+
+detkit)
+    cd $SCRIPT_DIR/build
+    rm -rf *
+    cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_DIR/cmake/DevkitArm3DS-toolchain.cmake" -DNCNN_DIR="$SCRIPT_DIR/lib/build/ncnn_3ds/lib/cmake/ncnn" $SCRIPT_DIR/lib/detector_c
+    make -j6 install
+    mv install $SCRIPT_DIR/lib/build/detector
+    rm -rf *
+    ;;
+
+clean)
+    rm -rf $SCRIPT_DIR/build
+    rm -rf $SCRIPT_DIR/lib/build
+esac 
+}
+
 mkdir -p $SCRIPT_DIR/build
-cd $SCRIPT_DIR/build
-wget https://github.com/lvgl/lvgl/archive/refs/tags/v8.3.11.zip
-unzip v8.3.11.zip
-mv lvgl-8.3.11 $SCRIPT_DIR/lib/lvgl/lvgl-8.3.11
+mkdir -p $SCRIPT_DIR/lib/build
 
-mkdir -p $SCRIPT_DIR/lib/build/ncnn_3ds
-cd $SCRIPT_DIR/lib/build/ncnn_3ds
-wget https://github.com/Deepdive543443/Benchncnn-3DS/releases/download/20240410/ncnn_3ds_20240410.zip
-unzip ncnn_3ds_20240410.zip
-rm ncnn_3ds_20240410.zip
-
-cd $SCRIPT_DIR/build
-cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_DIR/cmake/DevkitArm3DS-toolchain.cmake" -DNCNN_DIR="$SCRIPT_DIR/lib/build/ncnn_3ds/lib/cmake/ncnn" $SCRIPT_DIR/lib/detector_c
-make -j6 install
-
-rm -rf *
-cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_DIR/cmake/DevkitArm3DS-toolchain.cmake" $SCRIPT_DIR/lib/lvgl
-make -j6 install
-
-rm -rf *
+build_module "$1"
+build_module "$2"
+build_module "$3"
+# if [ "$" == "" ] 
+# then
+#   build_module("")
+#   build_module(lvgl)
+#   build_module(ncnn)
+#   build_module(detkit)
+# else
+#   build_module(detkit)
+# fi
