2ccf29ea64871c3212227112d06161e603477cdb
JustinFung <835776509@qq.com>
2024-07-23 21:01:54 +0800

    Add submodules

--- setup.sh
+++ setup.sh
@@ -5,27 +5,34 @@ case "$1" in
 lvgl)
     cd $SCRIPT_DIR/build
     rm -rf *
-    wget https://github.com/lvgl/lvgl/archive/refs/tags/v8.3.11.zip
-    unzip v8.3.11.zip
-    mv lvgl-8.3.11 $SCRIPT_DIR/lib/lvgl/lvgl-8.3.11
-    
-    cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_DIR/cmake/DevkitArm3DS-toolchain.cmake" $SCRIPT_DIR/lib/lvgl
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_DIR/cmake/DevkitArm3DS-toolchain.cmake" $SCRIPT_DIR/lib/liblvgl
     make -j6 install
     mv install $SCRIPT_DIR/lib/build/lvgl
     rm -rf *
-    ;;
 
+    cd $SCRIPT_DIR
+    ;;
 ncnn)
-    mkdir -p $SCRIPT_DIR/lib/build/ncnn_3ds
-    cd $SCRIPT_DIR/lib/build/ncnn_3ds
-    wget https://github.com/Deepdive543443/Benchncnn-3DS/releases/download/20240410/ncnn_3ds_20240410.zip
-    unzip ncnn_3ds_20240410.zip
-    rm ncnn_3ds_20240410.zip
-    ;;
+    cd $SCRIPT_DIR/build
+    rm -rf *
 
+    cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_DIR/cmake/DevkitArm3DS-toolchain.cmake" \
+        -DNCNN_SIMPLEOCV=ON \
+        -DNCNN_OPENMP=OFF \
+        -DNCNN_SIMPLEVK=OFF \
+        -DNCNN_VFPV4=OFF \
+        $SCRIPT_DIR/lib/ncnn
+    make -j6 install
+    mv install $SCRIPT_DIR/lib/build/ncnn_3ds
+    rm -rf *
+
+    cd $SCRIPT_DIR
+    ;;
 detkit)
     cd $SCRIPT_DIR/build
     rm -rf *
+
     cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_DIR/cmake/DevkitArm3DS-toolchain.cmake" \
         -DNCNN_DIR="$SCRIPT_DIR/lib/build/ncnn_3ds/lib/cmake/ncnn" \
         -DBUILD_C=ON \
@@ -33,17 +40,52 @@ detkit)
     make -j6 install
     mv install $SCRIPT_DIR/lib/build/detector
     rm -rf *
-    ;;
 
+    cd $SCRIPT_DIR
+    ;;
+3dsx)
+    cd $SCRIPT_DIR/build
+    rm -rf *
+
+    cmake $SCRIPT_DIR
+    make -j6 install
+    cp $SCRIPT_DIR/build/3dsx/* $SCRIPT_DIR/out
+
+    cd $SCRIPT_DIR
+    ;;
+cia)
+    cd $SCRIPT_DIR/build
+    rm -rf *
+
+    cmake -DBUILD_CIA=ON $SCRIPT_DIR
+    make -j6 install
+    cp $SCRIPT_DIR/build/cia/* $SCRIPT_DIR/out
+    ;;
+test)
+    cd $SCRIPT_DIR/build
+    rm -rf *
+
+    cmake -DBUILD_TEST=ON $SCRIPT_DIR
+    make -j6 install
+    cp $SCRIPT_DIR/build/3dsx/* $SCRIPT_DIR/out
+    ;;
+all)
+    ./setup.sh lvgl ncnn detkit 3dsx cia
+    ;;
 clean)
     rm -rf $SCRIPT_DIR/build
+    rm -rf $SCRIPT_DIR/out
     rm -rf $SCRIPT_DIR/lib/build
+    ;;
 esac 
 }
 
 mkdir -p $SCRIPT_DIR/build
+mkdir -p $SCRIPT_DIR/out
 mkdir -p $SCRIPT_DIR/lib/build
 
 build_module "$1"
 build_module "$2"
 build_module "$3"
+build_module "$4"
+build_module "$5"
